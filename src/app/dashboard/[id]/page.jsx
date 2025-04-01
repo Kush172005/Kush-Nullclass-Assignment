8f132ec7b95ee0dc0f6ed52d47ce25b0991eeaa5
Kush Agarwal <agrawalkush783@gmail.com>
2025-04-02 01:19:52 +0530

    fixed some issues

--- src/app/dashboard/[id]/page.jsx
+++ src/app/dashboard/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion, AnimatePresence } from "framer-motion";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import {
     Search,
     Bell,
